86d084fb40d299e374ce47835b643a944ca5ce64
Hailey Miller <hailey.miller91@gmail.com>
2016-06-09 11:11:53 -0400

    add search result/no tour error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,45 @@
 class UsersController < ApplicationController
 
   def index
-  # @test = RSpotify::Artist.search('Rihanna').first.top_tracks(:US)[0]
-  # binding.pry
+
   end
 
+#find the artist & artist information on search
   def find_artist
     #Find Artist from Spotify
-    artistname = params[:q]
-    spotify_url = RSpotify::Artist.search(artistname).first
-    artist = spotify_url.name
+    @artistname = params[:q]
+    spotify_url = RSpotify::Artist.search(@artistname).first
 
-    #Find Upcoming Concerts from Bands in Town
-    bit_api = ENV['BIT_ID']
-    artistname_bit = params[:q].gsub(' ', '%20')
-    concerts = HTTParty.get('http://api.bandsintown.com/artists/'+artistname_bit+'/events.json?api_version=2.0&app_id='+bit_api)
-    results = [artist, concerts]
-    @artist = results[0]
-    @artist_img = spotify_url.images[2]['url']
-    @concerts = results[1]
+    #prevent no-method-for-nil-class-error if artist cannot be found
+    if spotify_url.nil?
+      spotify_url
+    else
+      @artist = spotify_url.name
+      #Find Upcoming Concerts from Bands in Town
+      bit_api = ENV['BIT_ID']
+      artistname_bit = params[:q].gsub(' ', '%20')
+      @concerts = HTTParty.get('http://api.bandsintown.com/artists/'+artistname_bit+'/events.json?api_version=2.0&app_id='+bit_api)
 
-    #Find Top 6 Tracks from Spotify for Concert Votes
+      #Find artist img from Spotify
+      @artist_img = spotify_url.images[2]['url']
 
-    @song_results = spotify_url.top_tracks(:US)[0..5]
+      #Find number of upcoming shows from BIT
+      @num_shows = @concerts.length
+
+      #Find Top 6 Tracks from Spotify for Concert Votes
+      @song_results = spotify_url.top_tracks(:US)[0..5]
+    end
+
     render "users/index"
   end
 
+#part of new user creation method
   def new
     #needs empty object to be initialized
     @user = User.new
   end
 
+#create a new user when user registers
   def create
      @user = User.new(user_params)
     if @user.save
@@ -40,6 +49,7 @@ class UsersController < ApplicationController
     end
   end
 
+#define strong params
   private
     def user_params
       params
