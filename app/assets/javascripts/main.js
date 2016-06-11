46c63d8d6b356c60eca5dd064046e75bfd3d50aa
Hailey Miller <hailey.miller91@gmail.com>
2016-06-11 19:13:14 -0400

    add height-alignment for song tracks, show/hide functionality for i'm-going btn

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
 
-//expand concert/top tracks details when
+//expand/collapse concert/top tracks details when
 //'I'm Going' is clicked
   $('.going').click(function(){
-    var $active = $('.going').find('.active');
+    var specificShow = $(this).attr('id');
+    var topTracks = "#tracks-for-"+$(this).attr('id');
+    var active = $('.collapse-wrapper').find('.active');
 
-    $('.top-tracks-search').stop().slideDown('slow').addClass('active');
+    if(specificShow === $(this).attr('id')){
+      $(''+topTracks+'').stop().slideDown('slow').addClass('active');
+      active.stop().slideUp().removeClass('active');
+    };
 
-    $active.stop().slideUp().removeClass('active');
 
 })//end concert details expansion
 
 //click to play audio samples from Spotify
-$('.listen').click(function(e){
+$('.listen').click(function(){
   //find the html id of each track
   var trackId = $(this).attr('id');
   //find the link from the 'value' attribute
@@ -45,22 +49,5 @@ var checkedBoxes = $('input[type="checkbox"]').click(function(){
   };
 
 })//end votes show/hide functionality
-
-//Add Row 1 & 2 classes to tracks
-//when laoded for styling
-
-//if this attr id is >=3, add class row-one,
-//else, add class row-two
-
-// $('.single-track').load(function(){
-//     $(this).hasClass('.listen')
-//   if ($(this).attr('id') === 'track-0'){
-//     $(this).addClass("row-one");
-//   }else{
-//     $('.single-track').addClass("row-two");
-//   }
-
-// })
-
 
 })//end wrapper function
