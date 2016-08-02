e06e5ecc294642d4510bda370beb194f90140236
Hailey Miller <hailey.miller91@gmail.com>
2016-08-01 20:23:45 -0400

    update landing page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get '/search', to: 'users#find_artist'
   get '/vote', to: 'users#save_song_votes'
 
-  root to: 'sessions#new'
+  root to: 'users#new'
   resources :users, only: [:index, :create, :new]
 end
